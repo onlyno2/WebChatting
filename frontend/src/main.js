2d8daa77ccf29da94f2c16c337a3c26230a478f7
Nguyen Kien <kiennd010498@gmail.com>
2019-12-16 10:06:47 +0700

    add vuetifyjs

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,14 +4,18 @@ import router from "./router";
 import store from "./store";
 import axios from "axios";
 import regeneratorRuntime from "regenerator-runtime";
+import Vuetify from "vuetify";
 
+Vue.use(Vuetify);
+export default new Vuetify({});
 Vue.use(regeneratorRuntime);
 Vue.config.productionTip = false;
-Vue.prototype.$axios = axios
+Vue.prototype.$axios = axios;
 
 new Vue({
   router,
   store,
-  axios,
+  axios, 
+  vuetify: new Vuetify(),
   render: h => h(App)
 }).$mount("#app");
