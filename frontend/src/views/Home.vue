076605d34d851b535e6ef4f07a4b9c515746f68c
Nguyen Kien <kiennd010498@gmail.com>
2019-12-17 11:35:55 +0700

    config i18n

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <!-- cái này cho xuống footer -->
+    <v-btn large color="error" @click="changeLocale('vi')">Vi</v-btn>
+    <v-btn large color="success" @click="changeLocale('en')">En</v-btn>
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <VFacebookLogin app-id="480114589280370" @login="test"></VFacebookLogin>
     <img
       src="https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=1615965675211633&height=50&width=50&ext=1577870214&hash=AeQGmQqVLwBozM86"
       alt="asd"
     />
-    <v-btn text large color="error">Hello</v-btn>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import i18n from "@/plugins/locales/i18n.js";
 import HelloWorld from "@/components/HelloWorld.vue";
 import { VFBLogin as VFacebookLogin } from "vue-facebook-login-component";
 
@@ -32,11 +35,14 @@ export default {
           {}
         )
         .then(response => {
-          this.$store.commit('token/TOKEN', response.data.data);
+          this.$store.commit("token/TOKEN", response.data.data);
         })
         .catch(error => {
           console.log(error);
         });
+    },
+    changeLocale(locale) {
+      i18n.locale = locale;
     }
   }
 };
