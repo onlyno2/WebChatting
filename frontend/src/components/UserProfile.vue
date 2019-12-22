787220da8044c079842f4b03653de5fb5b347033
Dantokoro <duongdang0508@gmail.com>
2019-12-21 22:30:02 -0800

    fix profile

--- frontend/src/components/UserProfile.vue
+++ frontend/src/components/UserProfile.vue
@@ -1,6 +1,9 @@
 <template>
   <v-card>
     <v-container>
+      <div class="purple lighten-5 text-center font-weight-bold display-2 profile">
+       <span>PROFILE</span>
+      </div>  
       <v-row align="center" justify="center">
         <v-col md="4" class="text-left">
           <v-avatar size="200">
@@ -13,19 +16,19 @@
           </v-avatar>
         </v-col>
         <v-col md="4" class="text-left">
-          <p>{{ data.name }}</p>
-          <p>{{ data.email }}</p>
+          <p class="border border-info rounded-pill">{{ data.name }}</p>
+          <p class="border border-info rounded-pill">{{ data.email }}</p>
         </v-col>
       </v-row>
       <v-row align="center" justify="center">
         <v-col md="4" class="text-left">
-          <p class="font-weight-bold">Like</p>
+          <p class="font-weight-bold">Like  <v-icon color="blue lighten-2">mdi-thumb-up</v-icon></p>
           <v-avatar color="blue lighten-3" size="200">
             <p class="display-3">{{ data.like }}</p>
           </v-avatar>
         </v-col>
         <v-col md="4" class="text-left">
-          <p class="font-weight-bold">Dislike</p>
+          <p class="font-weight-bold">Dislike  <v-icon color="red lighten-2">mdi-thumb-down</v-icon></p>
           <v-avatar color="red lighten-3" size="200">
             <p class="display-3">{{ data.dislike }}</p>
           </v-avatar>
@@ -51,5 +54,13 @@ export default {
   }
 };
 </script>
-
-<style></style>
+  
+<style scoped>
+p {
+    margin: 5px;
+    padding: 5px;
+  }
+.profile {
+  padding: 10px;
+}
+</style>
