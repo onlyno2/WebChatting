f5d91e9012e06c3f4fcd098d2faa286890cfa1fb
Nguyen Kien <kiennd010498@gmail.com>
2019-12-22 12:14:34 +0700

    add mdboostrap

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,6 +8,7 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,6 +8,10 @@ import Vuetify from 'vuetify';
 import i18n from '@/plugins/locales/i18n';
 import alertify from '@/plugins/alertify/alertify';
 import InfiniteLoading from 'vue-infinite-loading';
+import 'bootstrap-css-only/css/bootstrap.min.css'
+import 'mdbvue/lib/css/mdb.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import * as mdbvue from 'mdbvue';
 
 Vue.use(Vuetify);
 export default new Vuetify({});
@@ -17,6 +21,9 @@ Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
 
 Vue.use(InfiniteLoading);
+for (const component in mdbvue) {
+  Vue.component(component, mdbvue[component])
+}
 
 new Vue({
   router,
